eb0cd9d88da9b237600ae413bd96aafeec4fd127
sju01334 <sju01334@gmail.com>
2022-07-04 19:07:12 +0900

    백준 2753 문제풀이

---
+++ src/Level1_2/_2753.java
@@ -0,0 +1,18 @@
+package Level1_2;
+
+import java.util.Scanner;
+
+public class _2753 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if(num%4 == 0 && num%100 != 0 || num%400 == 0){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
+
+}
