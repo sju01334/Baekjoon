0120a2ed9745cc1acb057bdec87198cc184423bf
sju01334 <sju01334@gmail.com>
2022-07-05 20:34:53 +0900

    백준 11022 문제풀이

---
+++ src/Level1_3/_11022.java
@@ -0,0 +1,20 @@
+package Level1_3;
+
+import java.io.*;
+
+public class _11022 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < n; i++){
+            String s = bf.readLine();
+            int a = Integer.parseInt(s.split(" ")[0]);
+            int b = Integer.parseInt(s.split(" ")[1]);
+            bw.write("Case #"+(i+1)+": "+a+" + "+b+" = "+(a+b)+"\n");
+        }
+        bw.flush();
+
+    }
+}
