2809f7cbd429284c661a28e7503b1e2a41adc711
sju01334 <sju01334@gmail.com>
2022-07-05 20:49:39 +0900

    백준 2439 문제풀이

---
+++ src/Level1_3/_2439.java
@@ -0,0 +1,20 @@
+package Level1_3;
+
+import java.util.Scanner;
+
+public class _2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i = 0; i<num; i++){
+            for(int k = 1; k<num-i; k++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
