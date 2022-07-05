a885c29bcb85e6548143ea635d65a573fb5ea29f
sju01334 <sju01334@gmail.com>
2022-07-05 17:27:53 +0900

    백준 10950 문제풀이

---
+++ src/Level1_3/_10950.java
@@ -0,0 +1,24 @@
+package Level1_3;
+
+import java.util.Scanner;
+
+public class _10950 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+
+        for(int i = 0 ; i< num ; i++ ){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[i] = a+b;
+
+        }
+
+        for (int i : arr){
+            System.out.println(i);
+        }
+    }
+}
