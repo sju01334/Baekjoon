c3e2a6cafe42df1ecb488c05dffac0d1dda44cbf
sju01334 <sju01334@gmail.com>
2022-07-05 20:25:54 +0900

    백준 2742 문제풀이

---
+++ src/Level1_3/_2742.java
@@ -0,0 +1,15 @@
+package Level1_3;
+
+import java.util.Scanner;
+
+public class _2742 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i = num; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
