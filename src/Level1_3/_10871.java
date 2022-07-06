cdca12bc2d1760c7e2583c1d8a292d623408405c
sju01334 <sju01334@gmail.com>
2022-07-06 18:52:03 +0900

    백준 10871 문제풀이

---
+++ src/Level1_3/_10871.java
@@ -0,0 +1,24 @@
+package Level1_3;
+
+import java.util.Scanner;
+
+public class _10871 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int X = in.nextInt();
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] < X) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
